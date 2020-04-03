14e462d6021c0a7a837d5ada4897fd910dc7f866
irozajac1 <37371441+irozajac1@users.noreply.github.com>
2020-04-03 11:40:33 +0200

    zadatak 4

--- microservicequiz/src/main/java/com/example/mikroservisquiz/repository/doneQuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mikroservisquiz.repository;
-
-import com.example.mikroservisquiz.models.DoneQuiz;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface doneQuizRepository extends CrudRepository<DoneQuiz, Integer> {
-}
--- microservicequiz/src/main/java/com/example/mikroservisquiz/repository/quizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mikroservisquiz.repository;
-
-import com.example.mikroservisquiz.models.Quiz;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface quizRepository extends CrudRepository<Quiz, Integer> {
-}
