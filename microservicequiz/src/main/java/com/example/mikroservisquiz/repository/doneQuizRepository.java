985aff2ff142270e95b55d93ee42e5cad8e277b9
irozajac1 <37371441+irozajac1@users.noreply.github.com>
2020-05-14 09:36:22 +0200

    grpc client

--- microservicequiz/src/main/java/com/example/mikroservisquiz/repository/doneQuizRepository.java
+++ microservicequiz/src/main/java/com/example/mikroservisquiz/repository/doneQuizRepository.java
@@ -3,10 +3,11 @@ package com.example.mikroservisquiz.repository;
 import java.util.List;
 
 import com.example.mikroservisquiz.models.DoneQuiz;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface doneQuizRepository extends CrudRepository<DoneQuiz, Integer> {
+public interface doneQuizRepository extends JpaRepository<DoneQuiz, Integer> {
     List<DoneQuiz> findAll();
     //DoneQuiz findById(int id);
     //List<DoneQuiz> findByUserId(int userid);
--- microservicequiz/src/main/java/com/example/mikroservisquiz/repository/quizRepository.java
+++ microservicequiz/src/main/java/com/example/mikroservisquiz/repository/quizRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import com.example.mikroservisquiz.models.Quiz;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface quizRepository extends CrudRepository<Quiz, Integer> {
+public interface quizRepository extends JpaRepository<Quiz, Integer> {
     List<Quiz> findAll();
     Quiz findById(int id);
     //List<Quiz> findAllByResult(int result);
