153dcfeff65af10de5fe721f85d3a5b67e2b9c5d
slekic1 <slekic1@etf.unsa.ba>
2020-05-15 12:59:54 +0200

    grpc client added

--- microserviceuser/src/main/java/com/example/microserviceuser/repository/UserRepository.java
+++ microserviceuser/src/main/java/com/example/microserviceuser/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.example.microserviceuser.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.microserviceuser.models.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	//public User findUserByUsername(String username);
 
