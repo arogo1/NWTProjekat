72ad2ec591ced31ef75c8c8d40964d44159a01e5
arogo1 <arnes_rogo@hotmail.com>
2020-05-21 22:41:56 +0200

    Create component

--- Frontend/Frontend/src/app/app.module.ts
+++ Frontend/Frontend/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InquiryComponent } from './inquiry/inquiry.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InquiryComponent
+    InquiryComponent,
+    QuizComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, InquiryComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
